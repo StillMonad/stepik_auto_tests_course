e8bdd4eda50808ebfc1498250851b879521d53a8
Krio Andromeda <stillmonad@gmail.com>
2023-02-26 09:50:31 +0300

    project restructured

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+
+@pytest.fixture()
+def create_chrome(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
+
+@pytest.fixture()
+def browser():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+
+    yield browser
+    browser.quit()
